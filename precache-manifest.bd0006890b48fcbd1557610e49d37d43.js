22dc27a0505273afb942bfa19828ac5bee044eb9
Mostafizur Rahman <mostafizur.rahman@sourcetop.com>
2020-06-09 13:11:10 +0600

    Updates

---
+++ precache-manifest.bd0006890b48fcbd1557610e49d37d43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd08e807aba95a4cef54c5d1311e3e49",
+    "url": "/usercard/index.html"
+  },
+  {
+    "revision": "67ab45e4c56bed571633",
+    "url": "/usercard/static/css/main.6be79a3d.chunk.css"
+  },
+  {
+    "revision": "24b7520cc8aff6cc546d",
+    "url": "/usercard/static/js/2.c6fd865b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/usercard/static/js/2.c6fd865b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ab45e4c56bed571633",
+    "url": "/usercard/static/js/main.07af32d9.chunk.js"
+  },
+  {
+    "revision": "dc9594d85efc3fa4be55",
+    "url": "/usercard/static/js/runtime-main.a60fb4b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/usercard/static/media/logo.5d5d9eef.svg"
+  }
+]);
